a29996482adcd3091de9345aa8978cc2bea3bfc7
VENKATESHAWS <pushadapuvenkatesh@gmail.com>
2022-10-14 07:48:51 +0530

    git terraform code

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_cidr_block = "10.20.0.0/16"
+subnet_cidr_block = "10.20.0.0/24"
+ami = "ami-0ee23bfc74a881de5"
+instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr_block" {
+
+}
+
+variable "ami" {
+  
+}
+
+
+
+variable "subnet_cidr_block" {
+  
+}
+
+variable "instance_type" {
+  
+}
